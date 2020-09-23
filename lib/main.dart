8a06747045fafd9b1f9d1b8471fb493319ff7042
Maxwell Walters <max.walters10@gmail.com>
2020-09-23 15:39:12 -0400

    Adding uni_links implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,125 @@
-import 'package:dltest/myapp.dart';
 import 'package:dltest/screen2.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:uni_links/uni_links.dart';
+import 'package:flutter/services.dart' show PlatformException;
+
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: '/',
-    routes: {
-      '/': (context) => MyApp(),
-      '/second': (context) => SecondScreen(),
-    },
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => TestApp(),
+        '/second': (context) => SecondScreen(),
+      },
+    )
+  );
+}
+
+class TestApp extends StatefulWidget {
+  @override
+  _TestAppState createState() => _TestAppState();
+}
+
+enum UniLinksType { string, uri }
+
+class _TestAppState extends State<TestApp> {
+  StreamSubscription _sub;
+  UniLinksType _type = UniLinksType.string;
+  String _latestLink = 'Unknown';
+  Uri _latestUri;
+  final _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  @override
+  dispose() {
+    if (_sub != null) _sub.cancel();
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  initPlatformState() async {
+    await initPlatformStateForUriUniLinks();
+  }
+  
+  // An implementation using the [Uri] convenience helpers
+  initPlatformStateForUriUniLinks() async {
+    // Attach a listener to the Uri links stream
+    _sub = getUriLinksStream().listen((Uri uri) {
+      if (!mounted) return;
+      setState(() {
+        _latestUri = uri;
+        _latestLink = uri?.toString() ?? 'Unknown';
+      });
+    }, onError: (err) {
+      if (!mounted) return;
+      setState(() {
+        _latestUri = null;
+        _latestLink = 'Failed to get latest link: $err.';
+      });
+    });
+
+    // Attach a second listener to the stream
+    getUriLinksStream().listen((Uri uri) {
+      print('got uri: ${uri?.path} ${uri?.queryParametersAll}');
+    }, onError: (err) {
+      print('got err: $err');
+    });
+
+    // Get the latest Uri
+    Uri initialUri;
+    String initialLink;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      initialUri = await getInitialUri();
+      print('initial uri: ${initialUri?.path}'
+          ' ${initialUri?.queryParametersAll}');
+      initialLink = initialUri?.toString();
+    } on PlatformException {
+      initialUri = null;
+      initialLink = 'Failed to get initial uri.';
+    } on FormatException {
+      initialUri = null;
+      initialLink = 'Bad parse the initial link as Uri.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _latestUri = initialUri;
+      _latestLink = initialLink;
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Deep Link Test'),
+      ),
+      body: Center(
+        child: Text(
+          'This is the main screen of the app',
+          style: TextStyle(fontSize: 20.0)
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(
+            context, '/second');
+        },
+        child: Icon(Icons.arrow_forward),
+      ),
+    );
+  }
 }
